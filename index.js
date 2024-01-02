d5fda2eaef8c9d74b77c2f848486cbe70eff81a7
Ivona Cvija <ivca.cvija@gmail.com>
2024-01-02 02:49:10 +0000

    projectDCWA

---
+++ index.js
@@ -0,0 +1,199 @@
+var express = require('express')
+var mySQLDAO = require('./mySQLDAO.js')
+var mongoDAO = require('./mongoDAO')
+var app = express()
+let ejs = require('ejs');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+const { check, validationResult } = require('express-validator');
+var pool = require('./mySQLDAO.js').pool;
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/stores', (req, res) => {
+    console.log("Running /stores...")
+
+    mySQLDAO.getStores()
+        .then((data) => {
+            console.log(data)
+            res.render("stores", { "stores": data })
+        })
+        .catch((error) => {
+            res.send(error)
+        })
+})
+
+app.get('/products', (req, res) => {
+    console.log("Running /products...")
+
+    mySQLDAO.getProducts()
+        .then((data) => {
+            console.log(data)
+            res.render("products", { "products": data })
+        })
+        .catch((error) => {
+            res.send(error)
+        })
+})
+
+//open addStore
+app.get('/addStore', (req, res) => {
+    res.render("addStore", { errors: undefined });
+})
+
+//add store
+app.post('/addStore', [
+    check("sid").isLength({ min: 5, max: 5 }).withMessage("Store ID should have 5 characters"),
+    check("location").notEmpty().withMessage("Please enter Location"),
+    check("mgrid").isLength({ min: 4, max: 4 }).withMessage("Manager ID should have 4 characters")
+], async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        res.render("addStore", { errors: errors.errors });
+    } else {
+        console.log("Received values from the form:");
+        console.log("sid ", req.body.sid);
+        console.log("location ", req.body.location);
+        console.log("mgrid ", req.body.mgrid);
+
+        try {
+            // Insert the store information into the database
+            const [result] = await mySQLDAO.addStoreToDatabase(req.body.sid, req.body.location, req.body.mgrid);
+            console.log(result);
+            res.redirect('/stores');
+        } catch (error) {
+            console.error(error);
+            res.redirect('/stores');
+        }
+    }
+});
+
+//open editStore
+app.get('/stores/editStore/:sid', (req, res) => {
+    const sid = req.params.sid;
+
+    mySQLDAO.getStoreBySid(sid)
+        .then((store) => {
+            if (!store) {
+                res.status(404).send('Store not found');
+            } else {
+                res.render("editStore", { errors: undefined, store: store });
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.redirect('/');
+        });
+});
+
+//update store in a database
+app.post('/stores/editStore/:sid', [
+    check("location").notEmpty().withMessage("Please enter Location"),
+    check("mgrid").isLength({ min: 4, max: 4 }).withMessage("Manager ID should have 4 characters")
+], async (req, res) => {
+    const sid = req.params.sid;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        res.render("editStore", { errors: errors.errors, store: { sid, location: req.body.location, mgrid: req.body.mgrid } });
+    } else {
+        console.log("Received values from the form:");
+        console.log("sid ", sid);
+        console.log("location ", req.body.location);
+        console.log("mgrid ", req.body.mgrid);
+
+        try {
+            // Update the store information in the database
+            const [result] = await mySQLDAO.updateStore(req.body.location, req.body.mgrid, sid);
+            console.log(result);
+            res.redirect('/stores');
+        } catch (error) {
+            console.error(error);
+            res.redirect('/');
+        }
+    }
+});
+
+//delete product
+app.get('/products/delete/:pid', (req, res) => {
+    mySQLDAO.deleteProduct(req.params.pid)
+
+        .then((data) => {
+            console.log(data)
+            if (data.affectedRows > 0) {
+                console.log("Product deleted");
+            }
+            res.redirect('/products');
+        })
+        .catch((error) => {
+            console.log(error)
+            //res.render("errors", { "Product is currently in stores and cannot be deleted":  errorMessage  })
+            res.render("errors", { errors: [{ msg: "Error: Product " + req.body.pid + " is currently in stores and cannot be deleted" }] });
+            //res.redirect('/')
+        })
+})
+
+//get managers (MongoDB)
+app.get('/managers', (req, res) => {
+
+    mongoDAO.findAllManagers()
+        .then((data) => {
+            res.render("managers", { "managers": data })
+        })
+        .catch((error) => {
+        })
+})
+
+//open addManager
+app.get('/addManager', (req, res) => {
+    res.render("addManager", { errors: undefined });
+})
+
+//add manager (MongoDB)
+app.post('/addManager', [
+    check("_id").isLength({ min: 4, max: 4 }).withMessage("Manager ID has to have 4 characters"),
+    check("name").isLength({ min: 5 }).withMessage("Name has to be over 5 characters"),
+    check("salary").isInt({ min: 30000, max: 70000 })
+        .withMessage("Salary must be number between 30000 and 70000")
+], async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        res.render("addManager", { errors: errors.errors });
+    } else {
+        console.log("Received values from the form:");
+        console.log("_id ", req.body._id);
+        console.log("name ", req.body.name);
+        console.log("mgrid ", req.body.salary);
+
+        mongoDAO.addManager({
+            _id: req.body._id,
+            name: req.body.name,
+            salary: req.body.salary
+        })
+            .then((result) => {
+                console.log(result)
+                console.log("Manager added")
+                res.redirect('/managers')
+            })
+            .catch((error) => {
+                console.log(error)
+                if (error.code === 11000) {
+                    res.render("addManager", { errors: [{ msg: "Error: Manager " + req.body._id + " already exists" }] });
+                } else {
+                    res.render("errors", { "myerror": error });
+                }
+            })
+    }
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
---
+++ mySQLDAO.js
@@ -0,0 +1,124 @@
+var pool;
+var pmysql = require('promise-mysql')
+
+//add connection
+pmysql.createPool({
+    connectionLimit: 3,
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'proj2023'
+})
+    .then(p => {
+        pool = p
+    })
+    .catch(e => {
+        console.log("pool error:" + e)
+    })
+
+//methods
+//get a list of stores
+var getStores = function () {
+    return new Promise((resolve, reject) => {
+
+        pool.query('SELECT * FROM store')
+            .then((data) => {
+                resolve(data)
+            })
+            .catch(error => {
+                reject(error)
+            })
+    })
+}
+
+//get a list of products
+var getProducts = function () {
+    return new Promise((resolve, reject) => {
+
+        pool.query(`
+        SELECT p.pid, p.productdesc, ps.sid, IFNULL(s.location, "") AS location, IFNULL(ps.Price, "") AS Price
+            FROM product p
+            LEFT JOIN product_store ps ON p.pid = ps.pid
+            LEFT JOIN store s ON ps.sid = s.sid;`)
+            .then((data) => {
+                resolve(data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+//inserting store into a database
+var addStoreToDatabase = function (sid, location, mgrid) {
+    var takeInformation = {
+        sql: 'insert into store set sid=?, location=?, mgrid=?',
+        values: [sid, location, mgrid]
+    }
+
+    return new Promise((resolve, reject) => {
+
+        pool.query(takeInformation)
+            .then((data) => {
+                resolve(data)
+            })
+            .catch(error => {
+                reject(error)
+            })
+    })
+}
+
+//update store based on ID
+var updateStore = function (location, mgrid, sid) {
+    return new Promise((resolve, reject) => {
+        var updateQuery = {
+            sql: 'UPDATE store SET location=?, mgrid=? WHERE sid=?',
+            values: [location, mgrid, sid]
+        };
+
+        pool.query(updateQuery)
+            .then((result) => {
+                resolve(result);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+// Get store by SID
+var getStoreBySid = function (sid) {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * FROM store WHERE sid = ?', [sid])
+            .then((data) => {
+                resolve(data[0]); // Assuming you expect only one result
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+var deleteProduct = function (pid) {
+    console.log(pid)
+
+    return new Promise((resolve, reject) => {
+
+        //delete from product_store table
+        var myQuery = {
+            sql: 'DELETE FROM product WHERE pid=? AND NOT EXISTS (SELECT 1 FROM product_store WHERE pid =?)',
+            values: [pid, pid]
+        }
+        pool.query(myQuery)
+            .then((data) => {
+                resolve(data)
+            })
+            .catch(error => {
+                reject(error)
+            })
+
+    })
+
+}
+//exports
+module.exports = { getStores, getProducts, addStoreToDatabase, updateStore, getStoreBySid, deleteProduct }
